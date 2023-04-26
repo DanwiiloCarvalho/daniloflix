19c7033426bab5f4f0b749b2bfff5cb7df8cf99a
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-04-26 17:34:51 -0300

    Criação de um roteador inicial e inclusão do RouterProvider entre o Header e o Footer

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,5 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Homepage } from "../Pages/Homepage";
+import { MovieDetails } from "../Pages/MovieDetails";
+import { SearchResults } from "../Pages/SearchResults";
 
-const router = createBrowserRouter([
-    
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Homepage/>
+    },
+    {
+        path: '/movie/:movieid',
+        element: <MovieDetails/>
+    },
+    {
+        path: '/search',
+        element: <SearchResults/>
+    }
 ]);
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import { Header } from './Components/Header';
 import { Footer } from './Components/Footer';
+import { router } from './Router';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Header/>
-    
+    <RouterProvider router={router} />
     <Footer/>
   </React.StrictMode>
 )
