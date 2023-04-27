5fad538379b0082f30770ced15f8ecf386f85433
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-04-27 19:03:43 -0300

    Implementação do useEffect da Homepage para listagem dos filmes melhor avaliados

--- src/Components/MovieCard/index.tsx
+++ src/Components/MovieCard/index.tsx
@@ -1,14 +1,16 @@
+import { Movie } from '../../Pages/Homepage';
 import classes from './MovieCard.module.css';
 import { AiFillStar } from 'react-icons/ai';
 
-export function MovieCard() {
+export function MovieCard(props: Movie) {
+    const poster_path = import.meta.env.VITE_IMAGE + props.poster_path;
     return (
         <article className={classes.moviecard}>
-            <img className={classes.poster} src="https://image.tmdb.org/t/p/original/3bhkrj58Vtu7enYsRolD1fZdja1.jpg" alt="" />
-            <h1 className={classes.title}>The Godfather</h1>
+            <img className={classes.poster} src={poster_path} alt="" />
+            <h1 className={classes.title}>{props.title}</h1>
             <div className={classes.score}>
                 <span className={classes.star}><AiFillStar/></span>
-                <span className={classes.grade}>8.7</span>
+                <span className={classes.grade}>{props.vote_average}</span>
             </div>
             <button className={classes.details_button}>Detalhes</button>
         </article>
--- src/Pages/Homepage/index.tsx
+++ src/Pages/Homepage/index.tsx
@@ -1,29 +1,53 @@
-import { useEffect, useState } from 'react';
+import { ErrorInfo, useEffect, useState } from 'react';
 import { MovieCard } from '../../Components/MovieCard';
 import classes from './Homepage.module.css';
 
-type Movie = {
+export type Movie = {
     poster_path: string,
     title: string,
     vote_average: number
 }
 
+type DataResponse = {
+    page:number,
+    results: Movie[],
+    total_pages: number,
+    total_results: number
+}
+
 export function Homepage() {
 
-    const [moviesTopRated, setmoviesTopRated] = useState<Movie[] | null>(null);
+    const [moviesTopRated, setMoviesTopRated] = useState<Movie[] | null>(null);
+    const api:string = import.meta.env.VITE_API;
+    const api_key:string = import.meta.env.VITE_API_KEY;
+    const language:string = import.meta.env.VITE_API_LANGUAGE;
 
     useEffect(() => {
-        
+        async function fetchMovies() {
+            try {
+                const response = await fetch(api + '?' + api_key + '&' + language);
+                const data: DataResponse = await response.json();
+                const dataMovies = data.results.map(value => {
+                    return {
+                        poster_path: value.poster_path,
+                        title: value.title,
+                        vote_average: value.vote_average
+                    }
+                }); 
+                setMoviesTopRated(dataMovies);   
+            } catch (error: unknown) {
+                console.log(error as Error)
+            }
+        }
+        fetchMovies();
     },[])
 
     return (
         <main className={classes.main}>
             <h1>Melhores filmes:</h1>
             <ul className="movie-list">
-                {/* <li><MovieCard/></li>
-                <li><MovieCard/></li>
-                <li><MovieCard/></li> */}
-
+                {moviesTopRated 
+                    && moviesTopRated.map(movie => <li><MovieCard {...movie}/></li>)}
             </ul>
         </main>
     );
