bcd59ecf768a959967935d5072519ba42dba3bd1
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-04-27 17:46:54 -0300

    Criação de um arquivo com as variáveis de ambiente

--- src/Pages/Homepage/index.tsx
+++ src/Pages/Homepage/index.tsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from 'react';
 import { MovieCard } from '../../Components/MovieCard';
 import classes from './Homepage.module.css';
 
+type Movie = {
+    poster_path: string,
+    title: string,
+    vote_average: number
+}
+
 export function Homepage() {
+
+    const [moviesTopRated, setmoviesTopRated] = useState<Movie[] | null>(null);
+
+    useEffect(() => {
+        
+    },[])
+
     return (
         <main className={classes.main}>
             <h1>Melhores filmes:</h1>
             <ul className="movie-list">
+                {/* <li><MovieCard/></li>
                 <li><MovieCard/></li>
-                <li><MovieCard/></li>
-                <li><MovieCard/></li>
+                <li><MovieCard/></li> */}
+
             </ul>
         </main>
     );
