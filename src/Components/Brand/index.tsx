a13dba3564634e99835381b05cc9a398ec99d344
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-04-27 14:44:37 -0300

    Estilização mobile do cabeçalho

--- src/Components/Brand/index.tsx
+++ src/Components/Brand/index.tsx
@@ -1,8 +1,9 @@
 import { BiCameraMovie } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import classes from './Brand.module.css';
 
 export function Brand() {
     return (
-        <Link to={'/'}><BiCameraMovie/><span>Daniloflix</span></Link>
+        <Link to={'/'} className={classes.brand}><BiCameraMovie/><span>DaniloFlix</span></Link>
     );
 }
--- src/Components/Search/index.tsx
+++ src/Components/Search/index.tsx
@@ -1,8 +1,9 @@
 import { BiSearchAlt2 } from 'react-icons/bi';
+import classes from './Search.module.css';
 
 export function Search() {
     return (
-        <div>
+        <div className={classes.search}>
             <input type="text" placeholder="Busque um filme" />
             <button><BiSearchAlt2/></button>
         </div>
