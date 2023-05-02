9e135d075b6d968ebe4f193ac14219b02f4e6975
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2023-05-02 19:06:18 -0300

    Definição dos parâmetros de pesquisa na rota após clicar no botão pesquisar

--- src/Components/Search/index.tsx
+++ src/Components/Search/index.tsx
@@ -1,11 +1,41 @@
 import { BiSearchAlt2 } from 'react-icons/bi';
 import classes from './Search.module.css';
+import { useRef } from 'react';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 
 export function Search() {
+    const refWordKey = useRef<HTMLInputElement | null>(null);
+    //const [searchParams, setSearchParams] = useSearchParams();
+
+    /* function search() {
+        if (refWordKey.current) {
+            const params = {
+                q: refWordKey.current.value
+            }
+            setSearchParams(q);
+        } 
+    }*/
+    const navigate = useNavigate();
+
+    function search() {  
+        //navigate('/search?q=' + refWordKey.current?.value);
+        if (refWordKey.current?.value) {
+            const searchParams = createSearchParams({
+                q: refWordKey.current.value
+            });
+
+            navigate({
+                pathname: '/search',
+                search: `?${searchParams}`
+            });
+            refWordKey.current.value = "";
+        }
+    }
+
     return (
         <div className={classes.search}>
-            <input type="text" placeholder="Busque um filme" />
-            <button><BiSearchAlt2/></button>
+            <input ref={refWordKey} type="text" placeholder="Busque um filme" onKeyDown={e => e.key === 'Enter' ? search() : null}/>
+            <button onClick={search}><BiSearchAlt2/></button>
         </div>
     );
 }
--- src/Pages/SearchResults/index.tsx
+++ src/Pages/SearchResults/index.tsx
@@ -1,5 +1,15 @@
+import { useSearchParams } from 'react-router-dom';
+import classes from './SearchResults.module.css';
+
 export function SearchResults() {
+    const [searchParams] = useSearchParams();
+
     return (
-        <h1>Resultados para: </h1>
+        <main className={classes.main_container}>
+            <h1>Resultados para: {searchParams.get('q')}</h1>
+            <ul className={classes.results_search}>
+                {<li></li>}
+            </ul>
+        </main>
     );
 }
