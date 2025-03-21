3c5eebab71c31d3a30c558d55d87148095f509e3
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-03-21 19:01:42 -0300

    test: refactor the homepage E2E test by adding beforeEach function that visit '/' and intercept the 'Top Rated' API route

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -1,22 +1,18 @@
 describe('Homepage', () => {
 
-  it('should go to homepage from homepage when click on brand name', () => {
-    cy.visit("/");
-
+  beforeEach(() => {
+    cy.visit('/');
     cy.intercept('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=4a0342e2d6f23f9cacb905d34fd4ffcc&language=pt-BR', {
       fixture: 'bestMovies.json'
     });
+  })
 
+  it('should go to homepage from homepage when click on brand name', () => {
     cy.contains("DaniloFlix").click()
     cy.contains("Melhores filmes").and("be.visible")
   })
 
   it('should go to homepage from movie page when click on brand name', () => {
-    cy.visit("/");
-
-    cy.intercept('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=4a0342e2d6f23f9cacb905d34fd4ffcc&language=pt-BR', {
-      fixture: 'bestMovies.json'
-    });
 
     cy.intercept('GET', 'https://api.themoviedb.org/3/movie/278?api_key=4a0342e2d6f23f9cacb905d34fd4ffcc&language=pt-BR', {
       fixture: 'movieDetails.json'
@@ -32,29 +28,17 @@ describe('Homepage', () => {
   })
 
   it('should go to homepage from results page when click on brand name', () => {
-    cy.visit("/");
-
-    cy.intercept('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=4a0342e2d6f23f9cacb905d34fd4ffcc&language=pt-BR', {
-      fixture: 'bestMovies.json'
-    });
 
     cy.intercept('GET', 'https://api.themoviedb.org/3/search/movie?query=batman&api_key=4a0342e2d6f23f9cacb905d34fd4ffcc&language=pt-BR', {
       fixture: 'search.json'
     });
 
     cy.get("input[placeholder=\"Busque um filme\"]").type("batman{enter}");
-    
-
     cy.contains("DaniloFlix").click()
     cy.contains("Melhores filmes").and("be.visible")
   })
 
   it('should display 20 top rated movies from TMDB', () => {
-    cy.visit("/");
-    cy.intercept('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=4a0342e2d6f23f9cacb905d34fd4ffcc&language=pt-BR', {
-      fixture: 'bestMovies.json'
-    });
-
     cy.get("ul").children().should('have.length', 20);
   })
 
