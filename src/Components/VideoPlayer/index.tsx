8c8678c1a3765c739545b86b6ee6aea8e00ccf6d
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-04-07 18:22:22 -0300

    fix: change the CSS style import from the file "VideoPlayer.modules.css" to "VideoPlayer.module.css" in the VideoPlayer component

--- src/Components/VideoPlayer/index.tsx
+++ src/Components/VideoPlayer/index.tsx
@@ -1,7 +1,7 @@
-import './VideoPlayer.modules.css';
+import styles from './VideoPlayer.module.css';
 
 export function VideoPlayer(props: {key_trailer:string}) {
     return (
-        <iframe className="video" height="315" src={`https://www.youtube-nocookie.com/embed/${props.key_trailer}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe className={styles.video} height="315" src={`https://www.youtube-nocookie.com/embed/${props.key_trailer}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     );
 }
