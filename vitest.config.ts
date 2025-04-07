21d783aa2f84ce8e7ee21dcb2116ee0c2ecbb16a
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-04-07 10:37:00 -0300

    chore: add coverage exclude and environment variables into the vitest config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vitest/config'
+import { loadEnv } from 'vite'
 
 export default defineConfig({
   test: {
@@ -10,6 +11,17 @@ export default defineConfig({
     include: ['src/**/*.spec.tsx', 'src/**/*.spec.ts'],
     coverage: {
       provider: 'v8',
-    }
+      exclude: [
+        './src/main.tsx',
+        './src/Router',
+        './cypress',
+        './*.config.ts',
+        '.eslintrc.cjs',
+        './src/vite-env.d.ts',
+      ]
+    },
+    env: {
+      VITE_IMAGE: `https://image.tmdb.org/t/p/original`,
+    },
   },
 })
