7e154ad76b093927a9249c82b5fcaacfdd037d1a
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-04-25 20:12:52 -0300

    feat: transform the input of the Search component into a controlled input and fix aria-label from button

--- src/Components/Search/index.tsx
+++ src/Components/Search/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { createSearchParams, NavigateFunction } from 'react-router-dom';
 import classes from './Search.module.css';
@@ -8,6 +8,7 @@ interface SearchProps {
 }
 
 export function Search({ navigate }: SearchProps) {
+    const [inputValue, setInputValue] = useState<string>('');
     const refWordKey = useRef<HTMLInputElement | null>(null);
 
     function search() {  
@@ -26,8 +27,15 @@ export function Search({ navigate }: SearchProps) {
 
     return (
         <div className={classes.search}>
-            <input ref={refWordKey} type="text" placeholder="Busque um filme" onKeyDown={e => e.key === 'Enter' ? search() : null}/>
-            <button aria-label={`Buscar pelo filme ${refWordKey.current?.value}`} onClick={search}><BiSearchAlt2/></button>
+            <input
+                ref={refWordKey} 
+                type="text"
+                placeholder="Busque um filme"
+                value={inputValue}
+                onChange={(event) => setInputValue(event.target.value)}
+                onKeyDown={e => e.key === 'Enter' ? search() : null}
+            />
+            <button aria-label={`Buscar pelo filme${inputValue && ` ${inputValue}`}`} onClick={search}><BiSearchAlt2/></button>
         </div>
     );
 }
