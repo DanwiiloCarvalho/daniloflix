b0ae7671155114cf60bc66114245993799188e27
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-09-22 12:57:19 +0000

    chore: set the server.host option to 127.0.0.1 in the config to make it work for using a Dev Container

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/daniloflix/'
+  base: '/daniloflix/',
+  server: {
+    host: "127.0.0.1"
+  }
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+//   preview: {
+//     port: 4173,
+//     host: true
+//   }
+// })
